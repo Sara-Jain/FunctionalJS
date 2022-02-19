1d936a16f6669ff2567a4234edda33b036535dd9
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-19 09:40:47 +0530

    reformat: unit cases of higher order functions

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -1,18 +1,14 @@
 const repeat = (operation, num) => {
-    if (num > Number.MAX_SAFE_INTEGER) return "Number is too large!";
-    if (num <= 0 || typeof num !== 'number') return "Invalid input!";
+    if (num > Number.MAX_SAFE_INTEGER) return "Number out of range";
+    if (num <= 0 || typeof num !== 'number') return "Input format invalid.";
     else {
         // if(operation.arguments.length > 0) return 'Invalid function, enter no arguments!';
-        if(typeof operation !== 'function') return "Invalid, input a proper function!";
+        if(typeof operation !== 'function') return "Input format invalid.";
         operation();
         repeat(operation,--num);
     }
     return "Success!";
 }
-
-// module.exports = { 
-//     repeat
-// };
 
 // function repeat(operation, num) {
 //     if(num <= 0 ) return;
--- tests/higherOrderFunctions.test.js
+++ tests/higherOrderFunctions.test.js
@@ -2,41 +2,29 @@ const repeat = require('../higherOrderFunctions')
 // Success:
 // Should run the function n times if n (the input number) >0
 // Should run the function n times if it has no parameters 
-// Should run the function n times if no return value
 // Failure:
 // Should throw message when input number <=0
 // Should throw invalid message when input number is not numerical 
 // Should throw invalid message when input number is too large
-// Should throw invalid message if the function has a return type or parameters
 // Should throw invalid message when input function is not a function
 const funcCalled = () => {
     console.log("Function called");
-} 
+}
 describe('Repeat operation function', () => {
     it('should run the function n times if n (the input number)>0 and function called has no return value/parameters', () => {
-        expect(repeat(funcCalled,3)).toBe('Success!');
+        expect(repeat(funcCalled, 3)).toBe('Success!');
     });
-    // it('should run the function n times if it has no parameters or return value', () => {
-    //     expect(repeat(funcCalled ,2)).toBe("Success!");
-    // });
     it('should throw message when input number <=0', () => {
-        expect(repeat(funcCalled,-2)).toBe("Invalid input!");
+        expect(repeat(funcCalled, -2)).toBe("Input format invalid.");
     });
     it('should throw invalid message when input number is not numerical', () => {
-        expect(repeat(funcCalled,'hello')).toBe("Invalid input!");
+        expect(repeat(funcCalled, 'hello')).toBe("Input format invalid.");
     });
     it('should throw invalid message when input number is too large', () => {
-        expect(repeat(funcCalled, 99999999999999999999999999999999999999999999999999999999999909876546789654678654356787654356776543567)).toBe("Number is too large!");
+        expect(repeat(funcCalled, Number.MAX_SAFE_INTEGER + 1)).toBe("Number out of range");
     });
-    
+
     it('should throw invalid message when input function is not a function', () => {
-        expect(repeat(5,1)).toBe("Invalid, input a proper function!");
+        expect(repeat(5, 1)).toBe("Input format invalid.");
     });
-    // it('Should throw ivalid message if the function has a return type or parameters', () => {
-    //     funcResult = (num) => {
-    //         console.log(num);
-    //     }
-    //     const repeatResult = repeat(funcResult(5), 4);
-    //     expect(repeatResult).toBe("Invalid function, enter no arguments!"); 
-    // });
 });
