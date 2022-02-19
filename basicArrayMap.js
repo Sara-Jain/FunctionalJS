8b966a868d77d55968c3eb9c29df7d171d71c3dd
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-19 10:16:38 +0530

    fix: use array.some instead of array.every

--- basicArrayMap.js
+++ basicArrayMap.js
@@ -1,11 +1,8 @@
 module.exports = doubleAll = (array) => {
     if (array.constructor !== Array) return 'Invalid input format';
     if (array.length > 20) return 'Array size out of bounds';
-    if (array.every((num) => { return num > 9 || num < 0; })) return 'Array values out of range';
+    if (array.some((num) => { return num > 9 || num < 0; })) return 'Array values out of range';
     return array.map(number => number * 2);
-    // return array.map((num) => {
-    //     return num * 2;
-    // });
 };
 
 
